0029ae4f0370f237ee03b54bb069a19103b8ce64
Airidas Juska <airidas665@gmail.com>
2023-03-08 15:47:01 +0200

    write upd

--- r2/src/Use/useWrite.jsx
+++ r2/src/Use/useWrite.jsx
@@ -7,14 +7,37 @@ export const useWrite = _ => {
 
     const [response, setResponse] = useState(null);
     const [create, setCreate] = useState(null);
+    const [destroy, setDelete] = useState(null);
+    const [edit, setEdit] = useState(null);
 
     useEffect(() => {
+        if (null === edit) {
+            return;
+        }
+        axios.put(URL + '/' + edit.action + '/' + edit.id, { number: edit.number })
+            .then(res => setResponse(res.data));
+
+    }, [edit]);
+
+    useEffect(() => {
+        if (null === create) {
+            return;
+        }
         axios.post(URL, create)
             .then(res => setResponse(res.data));
 
     }, [create]);
 
+    useEffect(() => {
+        if (null === destroy) {
+            return;
+        }
+        axios.delete(URL + '/' + destroy.id)
+            .then(res => setResponse(res.data));
 
-    return [response, setCreate];
+    }, [destroy]);
+
+
+    return [response, setCreate, setEdit, setDelete];
 
 }
